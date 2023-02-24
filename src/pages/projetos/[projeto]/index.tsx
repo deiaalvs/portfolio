ac37ddfa09c6034e0fe25da4650955b83bf84f08
Andreia Costa <andreia_a.c@msn.com>
2023-02-24 19:26:44 -0300

    projectDetail page integration to prismic done

--- src/pages/projetos/[projeto]/index.tsx
+++ src/pages/projetos/[projeto]/index.tsx
@@ -1,34 +1,87 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import Prismic from '@prismicio/client';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import { Header } from '../../../components/Header';
 import { ProjectBanner } from '../../../components/ProjectBanner';
 import { ProjectDetailContainer } from '../../../styles/ProjectDetailStyles';
-import ecommerce from '../../../assets/ecommerce.png';
+import { getPrismicClient } from '../../../services/prismic';
 
-export default function ProjectDetail() {
+interface IProjectProps {
+  projectName: string;
+  projectType: string;
+  projectDescription: string;
+  projectLink: string;
+  projectImg: string;
+}
+
+interface IHomeProps {
+  projects: IProjectProps;
+}
+
+export default function ProjectDetail({ projects }: IHomeProps) {
+  function handleProjectView() {
+    window.open(projects.projectLink);
+  }
+
   return (
     <ProjectDetailContainer>
       <Header />
       <ProjectBanner
-        projectName="Projeto 01"
-        projectType="Ecommerce"
-        projectImg={ecommerce}
+        projectName={projects.projectName}
+        projectType={projects.projectType}
+        projectImg={projects.projectImg}
       />
       <main>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem,
-          incidunt. Voluptatum vero iure fugiat sapiente esse illum sunt in, ut
-          ipsa facilis maxime? Pariatur soluta explicabo placeat accusantium
-          quod quas officiis enim aspernatur sunt? Tenetur, distinctio.
-          Cupiditate, quia dolorum eius esse eum voluptas enim ad et quidem
-          corporis quis maxime soluta modi quas velit exercitationem voluptatem
-          itaque labore nesciunt vitae impedit ab dignissimos omnis iste?
-          Consectetur sequi praesentium deserunt aliquam quibusdam quod ab
-          debitis, porro neque itaque quaerat dicta adipisci.
-        </p>
-        <button type="button">
+        <p>{projects.projectDescription}</p>
+        <button type="button" onClick={handleProjectView}>
           <a href="#">Ver projeto online</a>
         </button>
       </main>
     </ProjectDetailContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const projects = await prismic.query([
+    Prismic.Predicates.at('document.type', 'portfolio_projects')
+  ]);
+
+  const paths = projects.results.map(project => ({
+    params: {
+      projeto: project.uid
+    }
+  }));
+
+  return {
+    paths,
+    fallback: true
+  };
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const { projeto } = context.params;
+
+  const projectResponse = await prismic.getByUID(
+    'portfolio_projects',
+    String(projeto),
+    {}
+  );
+
+  const projects = {
+    projectId: projectResponse.uid,
+    projectName: projectResponse.data.projectName,
+    projectType: projectResponse.data.projectType,
+    projectDescription: projectResponse.data.projectDescription,
+    projectLink: projectResponse.data.projectLink.url,
+    projectImg: projectResponse.data.projectImg.url
+  };
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 86400
+  };
+};
--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,38 +1,63 @@
+import Prismic from '@prismicio/client';
+import { GetStaticProps } from 'next';
 import { Header } from '../../components/Header';
 import ProjectItem from '../../components/ProjectItem/index';
 import { ProjetosContainer } from '../../styles/ProjetosStyles';
-import ecommerce from '../../assets/ecommerce.png';
+import { getPrismicClient } from '../../services/prismic';
 
-export default function Projetos() {
+interface IProjectProps {
+  projectId: string;
+  projectName: string;
+  projectType: string;
+  projectDescription: string;
+  projectLink: string;
+  projectImg: string;
+}
+
+interface IHomeProps {
+  projects: IProjectProps[];
+}
+
+export default function Projetos({ projects }: IHomeProps) {
   return (
     <ProjetosContainer>
       <Header />
       <main className="container">
-        <ProjectItem
-          projectName="Projeto 01"
-          projectType="Ecommerce"
-          projectLink="teste"
-          projectImg={ecommerce}
-        />
-        <ProjectItem
-          projectName="Projeto 02"
-          projectType="Ecommerce"
-          projectLink="teste"
-          projectImg={ecommerce}
-        />
-        <ProjectItem
-          projectName="Projeto 03"
-          projectType="Ecommerce"
-          projectLink="teste"
-          projectImg={ecommerce}
-        />
-        <ProjectItem
-          projectName="Projeto 04"
-          projectType="Ecommerce"
-          projectLink="teste"
-          projectImg={ecommerce}
-        />
+        {projects.map(project => (
+          <ProjectItem
+            key={project.projectId}
+            projectName={project.projectName}
+            projectType={project.projectType}
+            projectLink={project.projectId}
+            projectImg={project.projectImg}
+          />
+        ))}
       </main>
     </ProjetosContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const projectResponse = await prismic.query(
+    [Prismic.Predicates.at('document.type', 'portfolio_projects')],
+    { orderings: '[document.first_publication_date desc]' }
+  );
+
+  const projects = projectResponse.results.map(project => ({
+    projectId: project.uid,
+    projectName: project.data.projectName,
+    projectType: project.data.projectType,
+    projectDescription: project.data.projectDescription,
+    projectLink: project.data.projectLink.url,
+    projectImg: project.data.projectImg.url
+  }));
+
+  return {
+    props: {
+      projects
+    },
+    revalidate: 86400
+  };
+};
